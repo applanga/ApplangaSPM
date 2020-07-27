60efb70bd013647bb1ccc85edf3972e361682e1c
richard <elms>
2020-07-27 16:49:02 +0200

    Applanga

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "applangaSPM",
-            targets: ["applangaSPM"]),
+            name: "Applanga",
+            targets: ["Applanga"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
